9e6ff59b1a400f6e043bf0f119adb6c5b91182bd
Bradley Dice <bdice@bradleydice.com>
2019-03-18 18:42:32 -0400

    Add new exploration notebooks.

---
+++ airline_data/exploration/util.py
@@ -0,0 +1,5 @@
+import os
+
+def hdfs_fn(job, filename):
+    """Prepend a filename with the job's HDFS workspace directory path."""
+    return os.path.join(job._project.config['hdfs_dir'], job._id, filename)
