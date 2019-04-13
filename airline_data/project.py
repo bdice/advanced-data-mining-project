bde0c4efb84e5e5d15744dd380e858beccad79d5
Bradley Dice <bdice@bradleydice.com>
2019-04-13 11:49:40 -0400

    Add operations to compute and plot HON PageRanks.

--- airline_data/project.py
+++ airline_data/project.py
@@ -78,12 +78,6 @@ def has_itineraryfile(job):
     return job.isfile('all_hon_itineraries.txt')
 
 
-@Project.label
-def has_plots(job):
-    PLOTS = ('pr_1st_order.png',)
-    return all([job.isfile(fn) for fn in PLOTS])
-
-
 def find_spark():
     import findspark
     findspark.init('/usr/hdp/current/spark2-client')
@@ -106,6 +100,33 @@ def build_spark():
         # Hide irrelevant warnings caused by workers running Python 2
         spark.sparkContext.setLogLevel("ERROR")
         yield spark
+
+
+def fetch_geodata():
+    import pandas as pd
+    project = Project()
+
+    if not project.isfile('airport_geodata.csv'):
+        import urllib.request
+        url = 'https://datahub.io/core/airport-codes/r/airport-codes.csv'
+        urllib.request.urlretrieve(url, project.fn('airport_geodata.csv'))
+
+    print('Reading airport geodata...')
+    geodata = pd.read_csv(project.fn('airport_geodata.csv'))
+    geodata = geodata.dropna(subset=['iata_code'])
+    geodata = geodata[geodata['iso_country'] == 'US']
+    geodata = geodata[['name', 'iata_code', 'coordinates']].set_index('iata_code')
+    coords = geodata['coordinates'].str.split(', ', expand=True)
+    geodata['lon'] = pd.to_numeric(coords[0])
+    geodata['lat'] = pd.to_numeric(coords[1])
+    geodata = geodata.drop(columns=['coordinates'])
+
+    # Ignore airports with erroneous data near (0, 0)
+    geodata = geodata[geodata.lon < -65]
+
+    # Drop data for Alaska and Hawaii
+    geodata = geodata[geodata.lon > -130]
+    return geodata
 
 
 @Project.operation
@@ -284,9 +305,10 @@ def combine_itineraries(job):
 
 @Project.operation
 @Project.pre.after(combine_edgefile)
-@Project.post(has_plots)
-def make_plots(job):
+@Project.post.isfile('pr_1st_order.png')
+def plot_first_order_pagerank(job):
     import matplotlib
+    matplotlib.use('Agg')
     import matplotlib.colors as mcolors
     import matplotlib.patches as mpatches
     import matplotlib.pyplot as plt
@@ -295,29 +317,6 @@ def make_plots(job):
     import pandas as pd
     import cartopy.crs as ccrs
     import cartopy.io.shapereader as shpreader
-
-    project = Project()
-
-    if not project.isfile('airport_geodata.csv'):
-        import urllib.request
-        url = 'https://datahub.io/core/airport-codes/r/airport-codes.csv'
-        urllib.request.urlretrieve(url, project.fn('airport_geodata.csv'))
-
-    print('Reading airport geodata...')
-    geodata = pd.read_csv(project.fn('airport_geodata.csv'))
-    geodata = geodata.dropna(subset=['iata_code'])
-    geodata = geodata[geodata['iso_country'] == 'US']
-    geodata = geodata[['name', 'iata_code', 'coordinates']].set_index('iata_code')
-    coords = geodata['coordinates'].str.split(', ', expand=True)
-    geodata['lon'] = pd.to_numeric(coords[0])
-    geodata['lat'] = pd.to_numeric(coords[1])
-    geodata = geodata.drop(columns=['coordinates'])
-
-    # Ignore airports with erroneous data near (0, 0)
-    geodata = geodata[geodata.lon < -65]
-
-    # Drop data for Alaska and Hawaii
-    geodata = geodata[geodata.lon > -130]
 
     print('Reading edge list...')
     G = nx.read_weighted_edgelist(job.fn('all_edges.tsv'), create_using=nx.DiGraph())
@@ -329,7 +328,7 @@ def make_plots(job):
     pageranks.index = pd.to_numeric(pageranks.index)
     iatas = iatas.join(pageranks)
 
-    airports = pd.merge(iatas, geodata, left_on='IATA', right_index=True)
+    airports = pd.merge(iatas, fetch_geodata(), left_on='IATA', right_index=True)
     airports['PageRankPercentile'] = airports['PageRank'].rank(pct=True)
 
     print('Generating plot...')
@@ -383,6 +382,83 @@ def generate_hon(job):
     generate(job.fn('all_hon_itineraries.txt'),
              job.fn('hon_rules.txt'),
              job.fn('hon_network.txt'))
+
+
+@Project.operation
+@Project.pre.after(generate_hon)
+@Project.post.isfile('hon_pagerank.txt')
+def hon_pagerank(job):
+    from pyHON import pagerank
+    print('Computing HON PageRank for {year}Q{quarter}'.format(**job.sp))
+    pagerank(job.fn('hon_network.txt'),
+             job.fn('hon_pagerank.txt'))
+
+
+@Project.operation
+@Project.pre.after(hon_pagerank)
+@Project.post.isfile('pr_hon.png')
+def plot_hon_pagerank(job):
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.colors as mcolors
+    import matplotlib.patches as mpatches
+    import matplotlib.pyplot as plt
+    import networkx as nx
+    import numpy as np
+    import pandas as pd
+    import cartopy.crs as ccrs
+    import cartopy.io.shapereader as shpreader
+
+    print('Reading IATAs and PageRanks...')
+    pageranks = pd.read_csv(job.fn('hon_pagerank.txt'), header=None, names=['ID', 'PageRank'])
+    pageranks = pageranks.set_index('ID')
+    iatas = pd.read_csv(job.fn('airport_codes.csv'))
+    iatas['ID'] = pd.to_numeric(iatas['ID'])
+    iatas = iatas.set_index('ID')
+    iatas = pd.merge(iatas, pageranks, left_index=True, right_index=True)
+
+    airports = pd.merge(iatas, fetch_geodata(), left_on='IATA', right_index=True)
+    airports['PageRankPercentile'] = airports['PageRank'].rank(pct=True)
+
+    print('Generating plot...')
+    fig = plt.figure(figsize=(6, 4), dpi=400)
+    ax = fig.add_axes([0, 0, 1, 1], projection=ccrs.LambertConformal())
+    ax.set_extent([-128, -64, 22, 49], ccrs.Geodetic())
+
+    shapename = 'admin_1_states_provinces_shp'
+    states_shp = shpreader.natural_earth(resolution='110m',
+                                         category='cultural', name=shapename)
+
+    ax.background_patch.set_visible(False)
+    ax.outline_patch.set_visible(False)
+    plt.title('PageRanks from Higher-Order Network ({}Q{})'.format(job.sp.year, job.sp.quarter))
+
+    def colorize_state(geometry):
+        facecolor = (0.9375, 0.9375, 0.8594)
+        return {'facecolor': facecolor, 'edgecolor': 'black'}
+
+    ax.add_geometries(
+        shpreader.Reader(states_shp).geometries(),
+        ccrs.PlateCarree(),
+        styler=colorize_state)
+
+    xs = airports.lon.values
+    ys = airports.lat.values
+    colors = airports.PageRank.values
+    sizes = 10000*airports.PageRank.values
+
+    dots = ax.scatter(xs, ys, transform=ccrs.PlateCarree(), c=colors, s=sizes, alpha=0.8, zorder=10,
+                      norm=mcolors.LogNorm(vmin=1e-4, vmax=1e-1), cmap='viridis')
+
+    for row in airports.itertuples():
+        if row.PageRankPercentile > 0.90:
+            ax.annotate(row.IATA, (row.lon, row.lat), xycoords=ccrs.PlateCarree()._as_mpl_transform(ax),
+                        fontsize=40*row.PageRank**0.2, alpha=row.PageRankPercentile, zorder=11,
+                        ha='center', va='center')
+    cbax = fig.add_axes([0.9, 0.1, 0.03, 0.8])
+    plt.colorbar(dots, cax=cbax)
+    plt.savefig(job.fn('pr_hon.png'))
+    plt.close()
 
 
 if __name__ == '__main__':
